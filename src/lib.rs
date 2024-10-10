b462ca255cbbfa7256b94b7b82dab3ff26527050
kmtr <kmtr@outlook.com>
2024-10-10 10:24:20 +0900

    Update: misc

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,18 @@ extern crate regex;
 
 mod iso3166;
 
-use std::borrow::Cow;
 use std::error;
 use std::fmt;
+use std::sync::LazyLock;
+
 use regex::Regex;
-use iso3166::*;
+
+use crate::iso3166::*;
+
+static LAZY_REGEX_D: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"\D").unwrap());
+static LAZY_REGEX_0: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"^0+").unwrap());
+static LAZY_REGEX_8: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"^8+").unwrap());
+static LAZY_REGEX_89: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"^89+").unwrap());
 
 #[derive(Debug)]
 pub struct NotValidPhoneNumberError;
@@ -27,25 +34,24 @@ impl error::Error for NotValidPhoneNumberError {
     }
 }
 
-pub fn parse<'a>(number: &str, country: &str) -> Result<Cow<'a, str>, NotValidPhoneNumberError> {
+pub fn parse<'a>(number: &str, country: &str) -> Result<String, NotValidPhoneNumberError> {
     let number = number.trim();
     let country = country.trim();
-    let has_plus_sign = number.starts_with("+");
 
     // remove any non-digit character, included the +
-    let mut number: String = Regex::new(r"\D").unwrap().replace_all(number, "").to_string();
+    let mut number: String = LAZY_REGEX_D.replace_all(number, "").to_string();
     let mut iso3166 = get_iso3166_by_country(&country).ok_or(NotValidPhoneNumberError)?;
 
     if !vec![ISO3166_GA.alpha3, ISO3166_CI.alpha3, ISO3166_CG.alpha3].contains(&iso3166.alpha3) {
-        number = Regex::new(r"^0+").unwrap().replace_all(&number, "").to_string();
+        number = LAZY_REGEX_0.replace_all(&number, "").to_string();
     }
 
-    if iso3166.alpha3 == ISO3166_RU.alpha3 && number.len() == 11 &&
-       Regex::new(r"^89").unwrap().is_match(&number) {
-        number = Regex::new(r"^8+").unwrap().replace_all(&number, "").to_string()
+    if iso3166.alpha3 == ISO3166_RU.alpha3 && number.len() == 11 && LAZY_REGEX_89.is_match(&number)
+    {
+        number = LAZY_REGEX_8.replace_all(&number, "").to_string()
     }
 
-    if has_plus_sign {
+    if number.starts_with("+") {
         iso3166 = get_iso3166_by_number(&number).ok_or(NotValidPhoneNumberError)?;
     } else {
         if iso3166.phone_number_lengths.contains(&number.len()) {
@@ -53,25 +59,29 @@ pub fn parse<'a>(number: &str, country: &str) -> Result<Cow<'a, str>, NotValidPh
         }
     }
     if validate_phone_iso3166(&number, iso3166) {
-        return Ok(Cow::Owned("+".to_owned() + &number));
+        return Ok("+".to_owned() + number.as_str());
     }
     return Err(NotValidPhoneNumberError);
 }
 
-
 pub fn get_iso3166_by_country(country: &str) -> Option<&'static ISO3166> {
-    let country = country.to_uppercase();
-    let l = country.len();
-    match l {
+    match country.len() {
         2 => {
-            return ISO3166S.into_iter().filter(|iso3166| iso3166.alpha2 == country).next();
+            return ISO3166S
+                .into_iter()
+                .filter(|iso3166| iso3166.alpha2 == country.to_uppercase())
+                .next();
         }
         3 => {
-            return ISO3166S.into_iter().filter(|iso3166| iso3166.alpha3 == country).next();
+            return ISO3166S
+                .into_iter()
+                .filter(|iso3166| iso3166.alpha3 == country.to_uppercase())
+                .next();
         }
         l if 4 < l => {
-            return ISO3166S.into_iter()
-                .filter(|iso3166| iso3166.country_name.to_uppercase() == country)
+            return ISO3166S
+                .into_iter()
+                .filter(|iso3166| iso3166.country_name.to_uppercase() == country.to_uppercase())
                 .next();
         }
         _ => return None,
@@ -80,13 +90,13 @@ pub fn get_iso3166_by_country(country: &str) -> Option<&'static ISO3166> {
 
 pub fn get_iso3166_by_number(number: &str) -> Option<&'static ISO3166> {
     for phone in ISO3166S {
-        let r = Regex::new(("^".to_owned() + phone.country_code).as_str()).unwrap();
         for l in phone.phone_number_lengths {
-            if r.is_match(number) && number.len() == phone.country_code.len() + l {
-                for mbw in phone.mobile_begin_with {
+            if number.starts_with(phone.country_code) && number.len() == phone.country_code.len() + l {
+                for &mbw in phone.mobile_begin_with {
                     if Regex::new(("^".to_owned() + phone.country_code + mbw).as_str())
                         .unwrap()
-                        .is_match(number) {
+                        .is_match(number)
+                    {
                         return Some(phone);
                     }
                 }
@@ -106,7 +116,7 @@ pub fn validate_phone_iso3166(number: &str, iso3166: &ISO3166) -> bool {
     for l in iso3166.phone_number_lengths {
         if l == &number.len() {
             for mbw in iso3166.mobile_begin_with {
-                if Regex::new(("^".to_owned() + mbw).as_str()).unwrap().is_match(&number) {
+                if number.starts_with(mbw) {
                     return true;
                 }
             }
@@ -114,7 +124,6 @@ pub fn validate_phone_iso3166(number: &str, iso3166: &ISO3166) -> bool {
     }
     return false;
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -134,12 +143,12 @@ mod tests {
         let iso3166 = parse("090 0000 0000", "jp");
         match iso3166 {
             Ok(jp) => assert_eq!("+819000000000", jp),
-            Err(err) => std::panic::panic_any(err)
+            Err(err) => std::panic::panic_any(err),
         }
         let iso3166 = parse("892 3456 7890", "ru");
         match iso3166 {
             Ok(ru) => assert_eq!("+79234567890", ru),
-            Err(err) => std::panic::panic_any(err)
+            Err(err) => std::panic::panic_any(err),
         }
     }
 }
